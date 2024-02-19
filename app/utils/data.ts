c99875b4afcdfcd97668c167c172d2295d61fc3c
Samuel Ayetigbo <ayetigbosamuel01@gmail.com>
2024-02-19 09:38:22 +0100

    stage 1

---
+++ app/utils/data.ts
@@ -0,0 +1,21 @@
+import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { FaX } from "react-icons/fa6";
+
+export const socials = [
+  {
+    link: "",
+    Icon: FaFacebook,
+  },
+  {
+    link: "",
+    Icon: FaX,
+  },
+  {
+    link: "",
+    Icon: FaWhatsapp,
+  },
+  {
+    link: "",
+    Icon: FaInstagram,
+  },
+];
